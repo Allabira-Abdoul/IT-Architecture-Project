23d3962c1a16a55ceb6195e4e165c9cb7d8e54c5
Allabira-Abdoul <allabiracharafaddine@gmail.com>
2024-12-06 10:29:33 +0100

    API Gateway and service registry functional

---
+++ backend/api-gateway/src/main/java/com/group5/apigateway/config/AppConfig.java
@@ -0,0 +1,34 @@
+package com.group5.apigateway.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+
+public class AppConfig {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate template() {
+        return new RestTemplate();
+    }
+
+        @Bean
+    public CorsWebFilter corsWebFilter(){
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        //config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
